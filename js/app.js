a7d0fc467a76c47ce0b38a620f9240aec0077c15
andreyjacques <andreyjacques28@gmail.com>
2017-03-16 19:59:28 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,50 +1,83 @@
+//instancia de uma classe Anonima
 var app = new function () {
-    console.log("Instaciando app");
-    this.el = document.getElementById("users");
-    this.usuarios = ['João', 'Ze', 'Maria'];
+    console.log("Instancinado app 2");
+    this.elUsuarios = document.getElementById("usuarios");
+    this.usuarios = ['Jão', 'Zé', 'Maria'];
+    this.itemEditar = -1;
     this.contar = function (qtd) {
-        var el = document.getElementById("qtd");
-        var nome = "Usuário";
+        var elContador = document.getElementById("contador");
+        var nome = "Usuario";
         if (qtd) {
             if (qtd > 1) {
-                this.nome = "Usuários";
+                nome = "Usuários";
             }
-            el.innerHTML = qtd + ' ' + nome;
+            elContador.innerHTML = qtd + ' ' + nome;
+
         } else {
-            el.innerHTML = 'Sem ' + nome;
+            elContador.innerHTML = 'Sem ' + nome;
         }
     };
-    this.listaTodos = function () {
+    this.listarTodos = function () {
+        //gera String em formato tabela
         var dados = '';
-        if (this.usuarios.length > 0) {
-            for (i = 0; i < this.usuarios.length; i++) {
-                dados += '<tr>';
-                dados += '<td>' + this.usuarios[i] + '</td>';
-                dados += '<td><input type="button" onclick="app.excluir(' + i + ')" value="Remover"/></td>';
-                dados += '<td><input type="button" onclick="app.editar(' + i + ')" value="Editar"/></td>';
-                dados += '</tr>';
-            }
+        for (i = 0; i < this.usuarios.length; i++) {
+            dados += '<tr>';
+            dados += '<td>' + this.usuarios[i] + '</td>';
+            dados += '<td><button onclick="app.excluir(' + i + ')">excluir</button></td>';
+            dados += '<td><button onclick="app.editar(' + i + ')">editar</button></td>';
+            dados += "</tr>";
         }
+        //atualiza contador
         this.contar(this.usuarios.length);
-        return this.el.innerHTML = dados;
-    };
-    this.addNome = function () {
-        var nome = document.getElementById("nome").value;
-        if (nome)
-            this.usuarios.push(nome);
-        this.listaTodos();
+        //atualizar DOM
+        this.elUsuarios.innerHTML = dados;
     };
     this.excluir = function (item) {
+        //remove1 1 elemento
         this.usuarios.splice(item, 1);
-        this.listaTodos();
+        this.listarTodos();
+    }
+
+    this.salvar = function () {
+        //acessando o campo com o nome do usuarios
+        var elNome = document.getElementById("add-nome");
+        //pegando o texto preenchido
+        var usuario = elNome.value;
+        if (usuario) {
+            if (this.itemEditar == -1) {
+                //inserindo no vetor
+                this.usuarios.push(usuario);
+
+            } else {
+                //Altera quando tem alguem para alterar
+                this.usuarios.splice(this.itemEditar, 1, usuario.trim());
+                this.itemEditar = -1;
+            }
+            //Atualizando a tela
+            this.listarTodos();
+            this.novo();
+        }
+
     };
+    this.novo = function () {
+        this.itemEditar = -1;
+        var elNome = document.getElementById("add-nome");
+        elNome.value = "";
+    }
+
     this.editar = function (item) {
+        this.itemEditar = item;
+        //acessando nome do array
         var nome = this.usuarios[item];
-        var elNome = document.getElementById("nome");
-        if (item < this.usuarios.length) {
+        if (nome) {
+            //Acessando o elemento no DOM  
+            var elNome = document.getElementById("add-nome");
+            //preenchendo o form
             elNome.value = nome;
-        } else {
-            console.log("erro");
         }
     }
 };
+
+
+app.listarTodos();
+
